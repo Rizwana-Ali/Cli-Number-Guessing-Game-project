b7d513b89e96d17518eb4c7533b4ce88906dff56
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-05-31 01:42:45 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+
+import inquirer from  "inquirer";
+const computer_number=Math.floor(Math.random() * 10 +1);
+const result = await inquirer.prompt([
+    {
+ name:"userguessing_number",
+ typr:"number",
+ message:"please guess a number between 1-10:",       
+    },
+]);
+//condition
+if(result.userguessing_number === computer_number){
+    console.log("congragulation! you guess a right number");
+}
+else{
+console.log("wrong number, Try Again");
+}
